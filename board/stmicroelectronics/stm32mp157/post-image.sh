a9a82a01a3140fb6e2fd1caf3b76acab1dd678ee
Kory Maincent <kory.maincent@bootlin.com>
2021-07-26 10:56:46 +0200

    configs/st_demo: support external devicetrees for kernel, U-boot and TF-A
    configs/demo: enable external devicetree for Kernel, U-boot and TF-A.
    configs/demo: rename devicetree by the one exported by STM32CubeMX.
    board/st/overlay: update extlinux.conf with the new devicetree naming.
    board/st/post-image.sh: update the script to allow writing other TF-A
    devicetree in the final image
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>

--- board/stmicroelectronics/stm32mp157/post-image.sh
+++ board/stmicroelectronics/stm32mp157/post-image.sh
@@ -10,18 +10,18 @@ atf_image()
 {
 	local ATF_VARIABLES="$(sed -n 's/^BR2_TARGET_ARM_TRUSTED_FIRMWARE_ADDITIONAL_VARIABLES="\(.*\)"$/\1/p' ${BR2_CONFIG})"
 
-	if grep -Eq "DTB_FILE_NAME=stm32mp157c-dk2.dtb" <<< ${ATF_VARIABLES}; then
-		echo "tf-a-stm32mp157c-dk2.stm32"
-	elif grep -Eq "DTB_FILE_NAME=stm32mp157a-dk1.dtb" <<< ${ATF_VARIABLES}; then
-                echo "tf-a-stm32mp157a-dk1.stm32"
-	elif grep -Eq "DTB_FILE_NAME=stm32mp157a-avenger96.dtb" <<< ${ATF_VARIABLES}; then
-                echo "tf-a-stm32mp157a-avenger96.stm32"
-	fi
+	local DTB_NAME="$(sed -n 's/.*DTB_FILE_NAME=\([^ ]*\)/\1/p' <<< ${ATF_VARIABLES})"
+	local STM_NAME="tf-a-$(cut -f1 -d'.' <<< ${DTB_NAME}).stm32"
+	echo ${STM_NAME}
 }
 
 main()
 {
 	local ATFBIN="$(atf_image)"
+	if [ ! -e ${BINARIES_DIR}/${ATFBIN} ]; then
+		echo "Can not find ATF binary ${ATFBIN}"
+		exit 1
+	fi
 	local GENIMAGE_CFG="$(mktemp --suffix genimage.cfg)"
 	local GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 	local SCRIPT_PATH=$(dirname "$0")
