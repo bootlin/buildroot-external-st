d477850ae969c064c1257f86e3563b4a3ca05537
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2023-07-13 13:39:32 +0200

    board/stmicroelectronics/stm32mp1/post-build.sh: add symlinks to WiFi firmware
    Recent versions of the Broadcom WiFi driver first tries to load the
    WiFi firmware using a firmware file name suffixed by the compatible
    string of the platform, before falling back to the plain firmware
    name. In order to avoid strange looking firmware loading errors during
    the boot process, we create the appropriate symbolic links.
    
    As part of this, we slightly refactor the script into two
    functions. bash is now used instead of sh to be able to use
    pushd/popd.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- board/stmicroelectronics/stm32mp1/post-build.sh
+++ board/stmicroelectronics/stm32mp1/post-build.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-main()
+fixup_extlinux_dtb_name()
 {
 	local DTB_NAME="$(sed -n 's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="\(.*\)"$/\1/p' ${BR2_CONFIG})"
 	local EXTLINUX_PATH="${TARGET_DIR}/boot/extlinux/extlinux.conf"
@@ -10,8 +10,20 @@ main()
 	fi
 
 	sed -i -e "s/%DTB_NAME%/${DTB_NAME}/" ${EXTLINUX_PATH}
-
-	exit $?
 }
 
-main $@
+add_wifi_fw_symlinks()
+{
+	if ! grep -q "^BR2_PACKAGE_LINUX_FIRMWARE=y" ${BR2_CONFIG}; then
+		return
+	fi
+
+	pushd ${TARGET_DIR}/lib/firmware/brcm
+	for board in stm32mp157a-dk1 stm32mp157c-dk2 stm32mp157d-dk1 stm32mp157f-dk2 stm32mp135f-dk; do
+		ln -sf brcmfmac43430-sdio.bin brcmfmac43430-sdio.st,${board}-mx.bin
+	done
+	popd
+}
+
+fixup_extlinux_dtb_name $@
+add_wifi_fw_symlinks $@
