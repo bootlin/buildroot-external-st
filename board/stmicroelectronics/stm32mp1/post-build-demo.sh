b450e459ef6eccc3450dc48c3ac9046325b7f274
Kory Maincent <kory.maincent@bootlin.com>
2023-11-17 19:50:58 +0100

    board/st-demo: Add RAUC system.conf file, and RAUC version description
    Add rauc system.conf description file. Set compatible parameter ot the
    devicetree name within the post-build-demo script.
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>

--- board/stmicroelectronics/stm32mp1/post-build-demo.sh
+++ board/stmicroelectronics/stm32mp1/post-build-demo.sh
@@ -11,4 +11,20 @@ add_wifi_fw_symlinks()
 	popd
 }
 
+set_rauc_info()
+{
+	local DTB_NAME="$(sed -n 's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="\(.*\)"$/\1/p' ${BR2_CONFIG} | cut -d'.' -f1)"
+
+	# Set RAUC compatible description to the kernel devicetree name
+	sed -i -e "s/%RAUC_COMPATIBLE%/${DTB_NAME}/" ${TARGET_DIR}/etc/rauc/system.conf
+
+	# Pass VERSION as an environment variable (eg: export from a top-level Makefile)
+	# If VERSION is unset, fallback to the Buildroot version
+	local RAUC_VERSION=${VERSION:-${BR2_VERSION_FULL}}
+
+	# Create rauc version file
+	echo "${RAUC_VERSION}" > ${TARGET_DIR}/etc/rauc/version
+}
+
 add_wifi_fw_symlinks $@
+set_rauc_info $@
