0da26f08763f8c98c8f3ba546159a4dc42d133be
Kory Maincent <kory.maincent@bootlin.com>
2024-07-02 14:50:28 +0200

    board/st: configs/stm32mp2: Use common post-build and post-image scripts
    Use post-build and post-image scripts fro both type of stm32mpu
    architecure.
    Add flash.tsv config file, which is needed for post-image script.
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>

--- board/stmicroelectronics/common/post-build.sh
+++ board/stmicroelectronics/common/post-build.sh
@@ -2,7 +2,8 @@
 
 fixup_extlinux_dtb_name()
 {
-	local DTB_NAME="$(sed -n 's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="\(.*\)"$/\1/p' ${BR2_CONFIG})"
+	local DTB_NAME_PATH="$(sed -n 's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="\(.*\)"$/\1/p' ${BR2_CONFIG})"
+	local DTB_NAME="$(basename ${DTB_NAME_PATH})"
 	local EXTLINUX_PATH="${TARGET_DIR}/boot/extlinux/extlinux.conf"
 	if [ ! -e ${EXTLINUX_PATH} ]; then
 		echo "Can not find extlinux ${EXTLINUX_PATH}"
